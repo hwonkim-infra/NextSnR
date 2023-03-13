6cb35afc419ec718556651f1f080687b5222ef5d
hwonkim-infra <52963259+hwonkim-infra@users.noreply.github.com>
2023-03-13 14:06:56 +0900

    Update PSCviewFrame.js

--- src/sections/@dashboard/PSC/PSCviewFrame.js
+++ src/sections/@dashboard/PSC/PSCviewFrame.js
@@ -12,15 +12,15 @@ import {
   Card,
   CircularProgress,
   Grid,
-  Stack,Tab, Tabs,
+  Stack,
+  Tab,
+  Tabs,
   Typography,
 } from "@mui/material";
-import ArrowForwardIosSharpIcon from '@mui/icons-material/ArrowForwardIosSharp';
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-
+import ArrowForwardIosSharpIcon from "@mui/icons-material/ArrowForwardIosSharp";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 
 import useTabs from "@/hooks/useTabs";
-
 
 import Iconify from "@/components/Iconify";
 import parse from "html-react-parser";
@@ -35,60 +35,38 @@ const PSCviewFrame = ({ currentPSC }) => {
     {
       value: "northAmerica",
       title: "북미",
-      component: (
-        <>
-         {parse(currentPSC.region?.northAmerica)}
-        </>
-      ),
+      component: <>{parse(currentPSC.region?.northAmerica || "")}</>,
     },
     {
       value: "australia",
       title: "호주",
-      component: (
-        <>
-         {parse(currentPSC.region?.australia)}
-         
-        </>
-      ),
+      component: <>{parse(currentPSC.region?.australia || "")}</>,
     },
     {
-        value: "CUTR",
-        title: "CUTR",
-        component: (
-          <>
-           
-          </>
-        ),
-      },
-      {
-        value: "Brazil",
-        title: "브라질",
-        component: (
-          <>
-           
-          </>
-        ),
-      },{ 
-        value: "OtherRegion",
-        title: "기타 지역",
-        component: (
-          <>
-            
-          </>
-        ),
-      },
+      value: "CUTR",
+      title: "CUTR",
+      component: <>{parse(currentPSC.region?.CUTR || "")}</>,
+    },
+    {
+      value: "Brazil",
+      title: "브라질",
+      component: <>{parse(currentPSC.region?.brazil || "")}</>,
+    },
+    {
+      value: "OtherRegion",
+      title: "기타 지역",
+      component: <>{parse(currentPSC.region?.otherRegion || "")}</>,
+    },
   ];
 
   return (
     <Grid container spacing={2}>
       <Grid item xs={12} md={12}>
-      <Box sx={{ fontSize: "h4.fontSize" }}>{currentPSC.item}</Box>
-
+        <Box sx={{ fontSize: "h4.fontSize" }}>{currentPSC.item}</Box>
       </Grid>
 
       <Grid item xs={12} md={5}>
         <Card sx={{ p: 3 }}>
-        
           <Box sx={{ p: 2 }}>
             <Typography variant="overline" sx={{ color: "text.secondary" }}>
               Reference
@@ -102,30 +80,30 @@ const PSCviewFrame = ({ currentPSC }) => {
             <Typography variant="body2">{currentPSC.requirements}</Typography>
           </Box>
           <Box sx={{ p: 2 }}>
-          <Typography variant="overline" sx={{ color: "text.secondary" }}>
+            <Typography variant="overline" sx={{ color: "text.secondary" }}>
               Regional Standard
             </Typography>
-          <Tabs
-            allowScrollButtonsMobile
-            variant="scrollable"
-            scrollButtons="auto"
-            value={currentTab}
-            onChange={onChangeTab}
-          >
-            {REGION_TABS.map((tab) => (
-              <Tab
-                disableRipple
-                key={tab.value}
-                value={tab.value}
-                icon={tab.icon}
-                label={tab.title}
-              />
-            ))}
-          </Tabs>
-          {REGION_TABS.map((tab) => {
-            const isMatched = tab.value === currentTab;
-            return isMatched && <Box key={tab.value}>{tab.component}</Box>;
-          })}
+            <Tabs
+              allowScrollButtonsMobile
+              variant="scrollable"
+              scrollButtons="auto"
+              value={currentTab}
+              onChange={onChangeTab}
+            >
+              {REGION_TABS.map((tab) => (
+                <Tab
+                  disableRipple
+                  key={tab.value}
+                  value={tab.value}
+                  icon={tab.icon}
+                  label={tab.title}
+                />
+              ))}
+            </Tabs>
+            {REGION_TABS.map((tab) => {
+              const isMatched = tab.value === currentTab;
+              return isMatched && <Box key={tab.value}>{tab.component}</Box>;
+            })}
           </Box>
         </Card>
       </Grid>
@@ -135,18 +113,16 @@ const PSCviewFrame = ({ currentPSC }) => {
             <Accordion key={row?.subItem}>
               <AccordionSummary
                 variant="subtitle1"
-                sx={{ background: "#f2f2f2", p:1, m:1 }}
-                expandIcon={<ExpandMoreIcon />}                
+                sx={{ background: "#f2f2f2", p: 1, m: 1 }}
+                expandIcon={<ExpandMoreIcon />}
               >
                 <Typography>{row?.subItem}</Typography>
-              </AccordionSummary >
+              </AccordionSummary>
               <AccordionDetails>{parse(row?.subAction || "")}</AccordionDetails>
             </Accordion>
           ))}
         </Card>
       </Grid>
-
-    
     </Grid>
   );
 };
