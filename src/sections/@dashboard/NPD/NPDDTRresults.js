615d75129f8609da9c7a4857a3a0911786c55b08
hwonkim-infra <52963259+hwonkim-infra@users.noreply.github.com>
2023-06-06 14:53:46 +0900

    Update NPDDTRresults.js

--- src/sections/@dashboard/NPD/NPDDTRresults.js
+++ src/sections/@dashboard/NPD/NPDDTRresults.js
@@ -1,15 +1,13 @@
 import {
-  Box,
   CircularProgress,
   Grid,
   ToggleButton,
-  ToggleButtonGroup,
+  ToggleButtonGroup
 } from "@mui/material";
 
 import { useState } from "react";
-import { NPDtargetMarkets } from "./NPDtargetMarkets";
-import NPDSigns from "./NPDSigns";
 import NPDViewGroup from "./NPDViewGroup";
+import { NPDtargetMarkets } from "./NPDtargetMarkets";
 
 export default function NPDDTRResults({ currentNPD }) {
   if (!currentNPD) return <CircularProgress />;
@@ -20,14 +18,13 @@ export default function NPDDTRResults({ currentNPD }) {
 
   function handleNPDsByMarket(e) {
     let marketNPDs = e.target.value;
-    console.log(marketNPDs);
     setActiveMarket(marketNPDs);
 
     marketNPDs !== "All"
       ? setNpdDatas(
           currentNPD.filter(
             (item) =>
-              item.market === marketNPDs || item.market.includes(marketNPDs)
+              {return (item.market === marketNPDs || item.market.includes(marketNPDs) || item.market === "common")}
           )
         )
       : setNpdDatas(currentNPD);
@@ -54,24 +51,48 @@ export default function NPDDTRResults({ currentNPD }) {
       </ToggleButtonGroup>
       <Grid container spacing={2}>
         <Grid item xs={3}>
-          
-          <NPDViewGroup npdDatas={npdDatas} groupName="Access" group={"access"} />
+          <NPDViewGroup
+            npdDatas={npdDatas}
+            groupName="Access"
+            group={"access"}
+          />
 
-          <NPDViewGroup npdDatas={npdDatas} groupName="Structure" group={"structure"} />
+          <NPDViewGroup
+            npdDatas={npdDatas}
+            groupName="Structure"
+            group={"structure"}
+          />
         </Grid>
         <Grid item xs={3}>
-          <NPDViewGroup npdDatas={npdDatas} groupName="Operator Station" group={"operatorStation"} />
-          <NPDViewGroup npdDatas={npdDatas} groupName="Power Train" group={"powerTrain"} />
-
+          <NPDViewGroup
+            npdDatas={npdDatas}
+            groupName="Operator Station"
+            group={"operatorStation"}
+          />
+          <NPDViewGroup
+            npdDatas={npdDatas}
+            groupName="Power Train"
+            group={"powerTrain"}
+          />
         </Grid>
         <Grid item xs={3}>
-        <NPDViewGroup npdDatas={npdDatas} groupName="Electric System" group={"electric"} />
-        <NPDViewGroup npdDatas={npdDatas} groupName="Hydraulics" group={"hydraulics"} />
-
+          <NPDViewGroup
+            npdDatas={npdDatas}
+            groupName="Electric System"
+            group={"electric"}
+          />
+          <NPDViewGroup
+            npdDatas={npdDatas}
+            groupName="Hydraulics"
+            group={"hydraulics"}
+          />
         </Grid>
         <Grid item xs={3}>
-        <NPDViewGroup npdDatas={npdDatas} groupName="Market Specific" group={"certification"} />
-
+          <NPDViewGroup
+            npdDatas={npdDatas}
+            groupName="Market Specific"
+            group={"certification"}
+          />
         </Grid>
       </Grid>
     </>
