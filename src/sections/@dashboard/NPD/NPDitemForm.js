9fe6669c5851791a7280c959b02b231cd0fbccc6
hwonkim-infra <52963259+hwonkim-infra@users.noreply.github.com>
2023-06-25 07:45:57 +0900

    Update NPDitemForm.js

--- src/sections/@dashboard/NPD/NPDitemForm.js
+++ src/sections/@dashboard/NPD/NPDitemForm.js
@@ -5,20 +5,37 @@ import { useRouter } from "next/router";
 
 // utils
 
-import { useForm } from "react-hook-form";
+import { Controller, useForm } from "react-hook-form";
 
 import DeleteIcon from "@mui/icons-material/Delete";
 import { LoadingButton } from "@mui/lab";
-import { Button, Card, Grid, Snackbar, Stack, Typography } from "@mui/material";
+import {
+  Button,
+  Card,
+  Checkbox,
+  Grid,
+  Snackbar,
+  Stack,
+  Typography,
+} from "@mui/material";
 
 import axios from "axios";
 import NPDitemInputs from "./NPDitemInputs";
+import { NPDtargetMarkets } from "./NPDtargetMarkets";
+
+NPDtargetMarkets.shift();
+console.log("🚀 ~ file: NPDitemForm.js:25 ~ NPDtargetMarkets:", NPDtargetMarkets)
+
+
+// const targetMarkets = NPDtargetMarkets.splice(1,-1)
+// console.log("🚀 ~ file: NPDitemForm.js:29 ~ targetMarkets:", targetMarkets)
 
 const NPDitemForm = ({ isEdit = false, currentNPDitem }) => {
   const {
     control,
     handleSubmit,
     setValue,
+    register,
     watch,
     reset,
     formState: { isSubmitting },
@@ -63,7 +80,7 @@ const NPDitemForm = ({ isEdit = false, currentNPDitem }) => {
       });
   };
 
-  const createNPDitem = async (values) => {    
+  const createNPDitem = async (values) => {
     axios
       .post("/api/NPDitem/EU", values)
       .then((response) => {
@@ -74,9 +91,7 @@ const NPDitemForm = ({ isEdit = false, currentNPDitem }) => {
       });
   };
 
-  
   async function removeNPDitem() {
-    const { id } = query;
     if (window.confirm("이 파일을 삭제하시겠습니까")) {
       try {
         axios
@@ -95,57 +110,66 @@ const NPDitemForm = ({ isEdit = false, currentNPDitem }) => {
     }
   }
 
-  return (
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <Grid container spacing={2}>
-          <Grid item xs={12} md={5}>
-            <Card sx={{ p: 1 }}>
-              <NPDitemInputs control={control} />
+  // console.log("🚀 ~ file: NPDitemForm.js:99 ~ NPDitemForm ~ targetMarkets:", targetMarkets)
 
-              <Stack
-                direction="row"
-                justifyContent="space-between"
-                alignItems="flex-end"
-                sx={{ mt: 3 }}
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <Grid container spacing={2}>
+        <Grid item xs={12} md={5}>
+          <Card sx={{ p: 1 }}>
+            <NPDitemInputs control={control} />
+            {NPDtargetMarkets.map(value => (
+              <Checkbox
+                key={value}
+                value={value}
+                {...register("market")}
+              />
+            ))}
+
+            <Stack
+              direction="row"
+              justifyContent="space-between"
+              alignItems="flex-end"
+              sx={{ mt: 3 }}
+            >
+              <LoadingButton
+                type="submit"
+                variant="contained"
+                loading={isSubmitting}
+                onClick={snackbarClick}
               >
-                <LoadingButton
-                  type="submit"
-                  variant="contained"
-                  loading={isSubmitting}
-                  onClick={snackbarClick}
-                >
-                  {!isEdit ? "Create File" : "Save Changes"}
-                </LoadingButton>
-                <Snackbar
-                    open={snackbarOpen}
-                    autoHideDuration={3000}
-                    message="This File was updated successfully"
-                    onClose={snackbarClose}
-                  />
-                <Button
-                  variant="outlined"
-                  startIcon={<DeleteIcon />}
-                  onClick={removeNPDitem}
-                >
-                  삭제
-                </Button>
-              </Stack>
-            </Card>
-          </Grid>
-          <Grid item xs={12} md={7}>
-            <Card sx={{ p: 1 }}>
-              <Typography
-                paragraph
-                variant="overline"
-                sx={{ color: "text.disabled" }}
+                {!isEdit ? "Create File" : "Save Changes"}
+              </LoadingButton>
+              <Snackbar
+                open={snackbarOpen}
+                autoHideDuration={3000}
+                message="This File was updated successfully"
+                onClose={snackbarClose}
+              />
+              <Button
+                variant="outlined"
+                startIcon={<DeleteIcon />}
+                onClick={removeNPDitem}
               >
-                Actions Detail
-              </Typography>
-        <pre>{JSON.stringify(values, 0, 2)}</pre>
-            </Card>
-          </Grid>
+                삭제
+              </Button>
+            </Stack>
+          </Card>
         </Grid>
-      </form>
+        <Grid item xs={12} md={7}>
+          <Card sx={{ p: 1 }}>
+            <Typography
+              paragraph
+              variant="overline"
+              sx={{ color: "text.disabled" }}
+            >
+              Actions Detail
+            </Typography>
+            <pre>{JSON.stringify(values, 0, 2)}</pre>
+          </Card>
+        </Grid>
+      </Grid>
+    </form>
   );
 };
 
